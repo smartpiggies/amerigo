486aa10a7cef9752b639c3350990053e672f53c1
tobyjaguar <talgya@gmail.com>
2019-04-25 13:20:11 -0400

    update Fetch and Display components

--- explorer/src/components/FetchData/index.js
+++ explorer/src/components/FetchData/index.js
@@ -10,8 +10,12 @@ class FetchData extends Component {
     super(props)
 
     this.hex2a = this.hex2a.bind(this)
+    this.fetchPiggies = this.fetchPiggies.bind(this)
+
     this.state = {
       piggies: [],
+      oracles: [],
+      data: []
     }
 
   }
@@ -24,15 +28,17 @@ class FetchData extends Component {
       return str;
   }
 
-  componentDidMount() {
+  fetchPiggies() {
     let address = "0"
     let underlying = "none"
+
     fetch(endpoint)
     .then(result => {
       return result.json()
     })
     .then(jsonResult => {
       let logData = jsonResult.data.map((item, i) => {
+
         fetch(item.relationships.transaction.links.related)
         .then(result => {
           return result.json()
@@ -41,38 +47,52 @@ class FetchData extends Component {
           address = result.data.attributes.msgPayload.inputs[2].value.slice(24)
           let oracleEndpoint = 'https://api.goerli.aleth.io/v1/contracts/' + address
 
-          return fetch(oracleEndpoint)
+          fetch(oracleEndpoint)
           .then(result => {
             return result.json()
           })
           .then(result => {
-            console.log(this.hex2a(result.data.attributes.constructorArgs[10]))
-            return underlying = this.hex2a(result.data.attributes.constructorArgs[10])
+            let array = this.state.oracles
+            array.push(
+              {
+                address: address,
+                underlying: this.hex2a(result.data.attributes.constructorArgs[10]),
+                url: this.hex2a(result.data.attributes.constructorArgs[8]),
+              }
+            )
+            this.setState({
+              oracles: array
+            })
           })
-        })
-        return(
-          {
-            index: i,
-            id: item.id,
-            topic: item.attributes.eventDecoded.topic0,
-            writer: item.attributes.eventDecoded.inputs[0].value,
-            piggyId: item.attributes.eventDecoded.inputs[1].value,
-            strike: item.attributes.eventDecoded.inputs[2].value,
-            expiry: item.attributes.eventDecoded.inputs[3].value,
-            rfp: item.attributes.eventDecoded.inputs[4].value,
-            txUrl: item.relationships.transaction.links.related,
-            oracle: address,
-            underlying: underlying,
-            url: "none"
-          }
-        )
 
+        })
+
+          return(
+            {
+              index: i,
+              id: item.id,
+              topic: item.attributes.eventDecoded.topic0,
+              writer: item.attributes.eventDecoded.inputs[0].value,
+              piggyId: item.attributes.eventDecoded.inputs[1].value,
+              strike: parseInt(item.attributes.eventDecoded.inputs[2].value, 16),
+              expiry: parseInt(item.attributes.eventDecoded.inputs[3].value, 16),
+              rfp: item.attributes.eventDecoded.inputs[4].value,
+              txUrl: item.relationships.transaction.links.related,
+              oracle: address,
+              underlying: underlying,
+              url: "none"
+            }
+          )
+
+        })
+        this.setState({
+          piggies: logData
+        })
       })
-      console.log(logData)
-      this.setState({
-        piggies: logData
-      })
-    })
+  }
+
+  componentDidMount() {
+    this.fetchPiggies()
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -84,38 +104,31 @@ class FetchData extends Component {
   }
 
   render() {
+    //console.log(this.state.oracles)
     //console.log(this.state.piggies[0])
-    let record, piggy, underlying, strike, expiry
-    if (this.state.piggies[0] !== undefined) {
-      //console.log(this.state.piggies[0].index)
-      record = this.state.piggies[0].id
-      piggy = this.state.piggies[0].piggyId
-      underlying = "none"
-      strike = parseInt(this.state.piggies[0].strike, 16)
-      expiry = parseInt(this.state.piggies[0].expiry, 16)
+    let tokens, filledUnderlying, filledURL
+
+    if (this.state.piggies[0] !== undefined && this.state.oracles[0] !== undefined) {
+      tokens = this.state.piggies.map((item) => {
+        if (this.state.oracles[item.index] !== undefined) {
+          filledUnderlying = this.state.oracles[item.index].underlying
+          filledURL = this.state.oracles[item.index].url
+        }
+        return <PiggyToken
+                  index={item.index}
+                  id={item.id}
+                  piggy={item.piggyId}
+                  asset={filledUnderlying}
+                  strike={item.strike}
+                  expiry={item.expiry}
+                  url={filledURL}
+              />
+      })
+
     }
-
-
     return (
       <div>
-      <ul>
-        <li>
-            {record}
-        </li>
-        <li>
-          ID: {piggy}
-        </li>
-        <li>
-          Underlying: {underlying}
-        </li>
-        <li>
-          Strike: {strike}
-        </li>
-        <li>
-          Expiration: {expiry}
-        </li>
-      </ul>
-      {/** {this.state.piggies[0].index} **/}
+      {tokens}
       </div>
     )
   }
--- explorer/src/components/PiggyToken/index.js
+++ explorer/src/components/PiggyToken/index.js
@@ -3,7 +3,6 @@ import React, { Component } from "react"
 class PiggyToken extends Component {
   constructor(props, context) {
     super(props)
-
   }
 
   componentDidMount() {
@@ -17,14 +16,23 @@ class PiggyToken extends Component {
 
   }
 
+  groomID(id) {
+    return id.slice(id.lastIndexOf("0")+1)
+  }
+
   render() {
     //console.log(this.state.piggyDetailMap)
+    //let groomedId = this.groomID(this.props.piggy)
 
     return (
       <div>
         <ul>
-          <li key={this.props.idx}>
-            {this.props.id}
+          <li key={this.props.index}>
+            ID: {this.groomID(this.props.piggy)} |
+            Underlying: {this.props.asset} |
+            Strike: {this.props.strike} |
+            Expiry: {this.props.expiry} |
+            URL: {this.props.url}
           </li>
         </ul>
       </div>
