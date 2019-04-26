c81ee8efda9140220d9715dcd7492892dac3ed77
tobyjaguar <talgya@gmail.com>
2019-04-26 12:34:16 -0400

    use explore with drizzle refactor

--- explorer/app/src/components/PiggyToken/index.js
+++ explorer/app/src/components/PiggyToken/index.js
@@ -1,26 +1,36 @@
 import React, { Component } from "react"
 import { drizzleConnect } from 'drizzle-react'
+import PropTypes from 'prop-types'
 
 //components
 import ExpansionPanel from '@material-ui/core/ExpansionPanel'
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
-import Typography from '@material-ui/core/Typography'
 import Divider from '@material-ui/core/Divider'
 import Grid from '@material-ui/core/Grid'
+
+import AddressItems from "../ListItems/AddressItems"
+import UintItems from "../ListItems/UintItems"
+import BoolItems from "../ListItems/BoolItems"
+
+let addressValues, uintValues, boolValues
 
 class PiggyToken extends Component {
   constructor(props, context) {
     super(props)
 
-    this.handleExpand = this.handleExpand.bind(this)
+    this.contracts = context.drizzle.contracts
+    this.drizzle = context.drizzle
+
+    //this.handleExpand = this.handleExpand.bind(this)
 
     this.state = {
-      panelOpen: false
+      dataKey: '0',
     }
   }
 
   componentDidMount() {
+
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -35,17 +45,29 @@ class PiggyToken extends Component {
     return id.slice(id.lastIndexOf("0")+1)
   }
 
-  handleExpand() {
-    this.setState({
-      panelOpen: !this.state.panelOpen
-    })
-    console.log(this.props.SmartPiggies)
+  handleExpand = (evt, expanded) => {
+
+    if (expanded) {
+      this.contracts.SmartPiggies.methods.getDetails(this.groomID(this.props.piggy)).call()
+      .then(result => {
+        if (result.length === 3) {
+          addressValues = <AddressItems item={result[0]} />
+          uintValues = <UintItems item={result[1]} />
+          boolValues = <BoolItems item={result[2]} />
+        }
+        //console.log(result[0])
+        //console.log(result[1])
+        //console.log(result[2])
+      })
+    }
+
   }
 
   render() {
     //console.log(this.state.piggyDetailMap)
     //let groomedId = this.groomID(this.props.piggy)
-
+    //console.log(this.props.SmartPiggies.getDetails[this.state.dataKey])
+    //console.log(this.state.dataKey)
     return (
       <div>
         <ExpansionPanel onChange={this.handleExpand}>
@@ -73,9 +95,9 @@ class PiggyToken extends Component {
               </Grid>
           </ExpansionPanelSummary>
           <ExpansionPanelDetails>
-            <Typography>
-              Hey! this is the piggy detail.
-            </Typography>
+              {addressValues}
+              {uintValues}
+              {boolValues}
           </ExpansionPanelDetails>
         </ExpansionPanel>
         <Divider />
@@ -84,8 +106,13 @@ class PiggyToken extends Component {
   }
 }
 
+PiggyToken.contextTypes = {
+  drizzle: PropTypes.object
+}
+
 const mapStateToProps = state => {
   return {
+    accounts: state.accounts,
     drizzleStatus: state.drizzleStatus,
     SmartPiggies: state.contracts.SmartPiggies
   }
